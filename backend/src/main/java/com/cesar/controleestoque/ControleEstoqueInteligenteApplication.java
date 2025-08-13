9bbda365b5bcc31355fb59278d0c211510d9ffbb
Rodrigo Nunes <nunes.rodrigo.dev@gmail.com>
2025-08-13 16:47:51 -0300

    Configuração inicial do projeto com Maven e dependências básicas
    Faltando apenas correções de erros ao tentar rodar no terminal

---
+++ backend/src/main/java/com/cesar/controleestoque/ControleEstoqueInteligenteApplication.java
@@ -0,0 +1,13 @@
+package com.cesar.controleestoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleEstoqueInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleEstoqueInteligenteApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cesar/controleestoque/ControleEstoqueInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesar.controleestoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleEstoqueInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
