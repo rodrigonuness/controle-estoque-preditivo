f3d154c6818a458f4c05c4a7a0fd15b567748613
Rodrigo Nunes <nunes.rodrigo.dev@gmail.com>
2025-08-18 16:38:14 -0300

    initialzr

---
+++ backend/src/main/java/com/estoque/preditivo/ControleDeEstoquePreditivoApplication.java
@@ -0,0 +1,13 @@
+package com.estoque.preditivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeEstoquePreditivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeEstoquePreditivoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/estoque/preditivo/ControleDeEstoquePreditivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estoque.preditivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeEstoquePreditivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
